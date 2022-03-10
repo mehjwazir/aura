0df0d0c37f2d8aa281285ddba1c509d46e1a9f34
mehjwazir <mehjwazir@gmail.com>
2022-03-10 01:42:29 -0800

    styled home & about pages

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,19 @@
+import './About.css';
+
+
 export default function About() {
 	return (
 		<main>
-			<h1>About</h1>
+			<h1 className='about'>ABOUT</h1>
+			<img src="https://i.imgur.com/2ntu0fr.jpg" alt="img" className="img-class"
+				width={300}/>
+			
+			<h3 className='h3aura'>REST <span className='aura'>AURA</span> NT</h3>
+			<p className="Text"> is a personal experience website that allows you to save your adventures in one place.</p>
+
+			<img src="https://i.imgur.com/mUCpU2K.jpg" alt="img" className='gray-img'
+				width={300} />
+
 		</main>
 	);
 }
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -7,9 +7,12 @@ export default function HomePage() {
 	return (
 		<main className="HomePage">
 			<div>
-				<h1>Home</h1>
-				<p>Your next dining adventure awaits..</p>
-				<Link to="/aura/experience/create" className="button btm-sm">Create Experience</Link>
+				<h1 className='title'>AURA</h1>
+				<p className='place'>Your next dining adventure awaits..</p>
+				<Link to="/aura/experience/create" className="button btm-sm createbtn">Create Experience</Link>
+			<div>
+					<img src="https://i.imgur.com/W4ska4Z.jpg" alt='img' width={300} className='rectangle-23' />
+			</div>
 			</div>
 		</main>
 
